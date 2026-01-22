f415dad335ffd16fbc1fb59227fcd2fc735610fb
work-krish <krisshhhp@gmail.com>
2026-01-22 17:26:11 +0530

    Custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ dotenv.config();
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 
 connectDB()
+.then(() =>{
+    app.listen(process.env.PORT ||8000 ,() =>{
+        console.log(`server is running at : ${process.env.port} || 8000`)
+    })
+})
+.catch((err) =>{
+    console.log("mongoDB connection faild", err );    
+})
