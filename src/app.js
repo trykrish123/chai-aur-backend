4aaa18264befd9e153ca04248a6cd911bc842b6f
work-krish <krisshhhp@gmail.com>
2026-01-24 11:20:05 +0530

    Complete guide for router and controller with debugging

--- src/app.js
+++ src/app.js
@@ -15,4 +15,15 @@ app.use(express.static('public'))
 
 app.use(cookieParser())
 
+
+// routes import
+
+import userRoutes from "./routes/user.routes.js"
+
+// routes declaration
+
+app.use("/api/v1/users", userRoutes)
+
+
+
 export { app }
